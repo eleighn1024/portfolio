66e666c0245fb6f13bc2cf667709db92ac929e19
eleighn1024 <eleabellejabuen@gmail.com>
2025-04-02 17:32:19 +0800

    test

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import '@fortawesome/free-regular-svg-icons'
+import '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import '../assets/styles/Timeline.scss'
+import '../assets/styles/Timeline.scss';
 
 function Timeline() {
   return (
@@ -15,15 +15,15 @@ function Timeline() {
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            contentArrowStyle={{ borderRight: '7px solid white' }}
             date="2023 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Western Institute of Technology</h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
+            <h4 className="vertical-timeline-element-subtitle">Bachelor of Science in Information Technology</h4>
             <p>
-        
+              Currently pursuing a degree in Information Technology, focusing on software development and data management.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -33,9 +33,9 @@ function Timeline() {
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Passi National High School</h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
+            <h4 className="vertical-timeline-element-subtitle">High School Diploma</h4>
             <p>
-              
+              Completed high school with a focus on science and mathematics, participating in various extracurricular activities.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -45,9 +45,9 @@ function Timeline() {
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Passi I Central School</h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
+            <h4 className="vertical-timeline-element-subtitle">Elementary Education</h4>
             <p>
-             
+              Completed elementary education with a strong foundation in basic subjects and social skills.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -57,16 +57,15 @@ function Timeline() {
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Pob. Ilaya Center</h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
+            <h4 className="vertical-timeline-element-subtitle">Preschool Education</h4>
             <p>
-              
+              Attended preschool, where I developed early learning skills and social interactions.
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
     </div>
-);
+  );
 }
-
 
 export default Timeline;
